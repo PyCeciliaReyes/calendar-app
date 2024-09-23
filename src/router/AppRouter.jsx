072cd06e669ffe625bba80f313c7239784a44e63
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-23 17:30:34 -0400

    configuracion de las rutas utilizando utilizando react-router-dom

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Router, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth';
+import { CalendarPage } from '../calendar';
+
+
+export const AppRouter = () => {
+    const authStatus = 'not-authenticated'; //'not-authenticated';
+    
+    return (
+        <Routes>
+            {
+                (authStatus==='not-authenticated')
+                    ? <Route path='/auth/*' element={<LoginPage/>} />
+                    : <Route path='/*' element={<CalendarPage/>} />
+            }
+           <Route path='/*' element={<Navigate to={'/auth/login'} />}/>
+
+        </Routes>
+  )
+}
